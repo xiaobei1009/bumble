f063777a9f74b99fceaf262228cf1f0a2b43519f
xiaobei1009 <xiaobei1009@163.com>
2018-11-16 18:02:18 +0800

    store dummy implementation

---
+++ bumble/bumble-store-dummy/src/main/java/org/bumble/store/dummy/App.java
@@ -0,0 +1,13 @@
+package org.bumble.store.dummy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
